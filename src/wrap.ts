d71e50fb0d9923347abfe5c4263f3c3d1197c1d8
homoluctus <w.slife18sy@gmail.com>
2021-07-01 16:30:17 +0900

    refactor(wrap): Change variable name to easy-to-understand one

--- src/wrap.ts
+++ src/wrap.ts
@@ -17,16 +17,16 @@ export function wrap(
       let row = rows.current as Row;
       const cnt = Math.ceil(strlen / maxColWidth);
       for (let j = 0; j < cnt; j++) {
-        const slicedStr = v.substring(j * maxColWidth, (j + 1) * maxColWidth);
+        const substr = v.substring(j * maxColWidth, (j + 1) * maxColWidth);
 
         if (j === 0) {
-          row.data[i] = slicedStr;
+          row.data[i] = substr;
         } else if (row.next && row.next.isChunk) {
-          row.next.data[i] = slicedStr;
+          row.next.data[i] = substr;
           row = row.next;
         } else {
           const old = row.next;
-          const chunk = createChunk(maxColWidths.length, i, slicedStr);
+          const chunk = createChunk(maxColWidths.length, i, substr);
           row.next = new Row(chunk, true);
           row.next.next = old;
           row = row.next;
